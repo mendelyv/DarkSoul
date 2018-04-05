0e81d55d8fd85b290f74b4c198e817cad54a7ad4
LORDRAN <1064137887@qq.com>
2018-04-05 13:44:49 +0800

    【简要描述】：首次创建工程 【详细描述】：新建Unity项目，尝试跟随傅老师学习使用Unity简易复刻黑魂 【修改人】：杨浩然

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    //====变量区
+
+    public string keyForward = "w";
+    public string keyBack = "s";
+    public string keyLeft = "a";
+    public string keyRight = "d";
+
+    public float dirForward;//前后方向的移动量
+    public float dirRight;//左右方向的移动量
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        dirForward = (Input.GetKey(keyForward) ? 1.0f : 0.0f) - (Input.GetKey(keyBack) ? 1.0f : 0.0f);
+        dirRight = (Input.GetKey(keyRight) ? 1.0f : 0.0f) - (Input.GetKey(keyLeft) ? 1.0f : 0.0f); 
+
+	}
+}
