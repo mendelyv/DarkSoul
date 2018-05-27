a6ff5d461e6568271c128e48f476d185f486e463
LORDRAN <1064137887@qq.com>
2018-05-27 21:30:43 +0800

    【简要描述】：进一步完善人物控制 【详细描述】： 	1.增加跑步动画，添加动画到ground混合树中 	2.使用球型插值法处理人物在旋转时速度过快的问题 	3.使用线性插值法处理人物从走到跑动画过渡 	4.使用公式解决斜向行走数值根号2的问题 	5.增加跳跃控制信号和跳跃动画 【修 改 人】：杨浩然

--- Assets/Scripts/Player/ActorController.cs
+++ Assets/Scripts/Player/ActorController.cs
@@ -7,6 +7,7 @@ public class ActorController : MonoBehaviour {
     public GameObject model;
     public PlayerInput pInput;
     public float walkSpeed = 2.0f;
+    public float runMultiplier = 2.0f;
 
     [SerializeField]
     private Animator anim;
@@ -23,10 +24,16 @@ public class ActorController : MonoBehaviour {
 
 	void Update () {
         //print(pInput.dirForward);
-        anim.SetFloat("forward", pInput.Dirmag);
+        anim.SetFloat("forward", pInput.Dirmag * Mathf.Lerp(anim.GetFloat("forward"), ((pInput.run) ? 2.0f : 1.0f),0.3f));
+        if (pInput.jump)
+            anim.SetTrigger("jump");
+
         if(pInput.Dirmag > 0.1f)
-            model.transform.forward = pInput.Dirvec;
-        movingVec = pInput.Dirmag * model.transform.forward * walkSpeed;
+        {
+            model.transform.forward = Vector3.Slerp(model.transform.forward, pInput.Dirvec, 0.25f); ;
+        }
+        movingVec = pInput.Dirmag * model.transform.forward * walkSpeed * ((pInput.run)?runMultiplier:1.0f);
+
 	}
 
     void FixedUpdate()
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -8,18 +8,34 @@ public class PlayerInput : MonoBehaviour
     public bool interactive = true;//是否可交互，控制模块的软开关
 
     #region====变量区
-
+    [Header("===== Key Setting =====")]
     public string keyForward = "w";
     public string keyBack = "s";
     public string keyLeft = "a";
     public string keyRight = "d";
 
-    public float dirForward;//前后方向的移动量
-    public float dirRight;//左右方向的移动量
+     public string keyA;
+     public string keyB;
+     public string keyC;
+     public string keyD;
 
-    public float Dirmag;//行走量
+    [Header("===== Output Signal =====")]
+    public float dirForward;//前后方向的移动量，随键盘按键时间的长短从0增加到1
+    public float dirRight;//左右方向的移动量，随键盘按键时间的长短从0增加到1
+
+    public float Dirmag;//两个方向键同时按下时的斜向行走量，注意根号2的问题
     public Vector3 Dirvec;//行走方向向量
 
+    // press signal
+    public bool run = false;
+
+    // trigger once signal
+    public bool jump = false;
+    public bool lastJump = false;
+
+    // double trigger
+
+    [Header("===== Others =====")]
     private float targetDirForward;//前后移动方向的目标值
     private float targetDirRight;//左右
     private float velocityForward;
@@ -42,13 +58,46 @@ public class PlayerInput : MonoBehaviour
             targetDirRight = 0.0f;
         }
 
+        //按方向键的输入量
         dirForward = Mathf.SmoothDamp(dirForward, targetDirForward, ref velocityForward, 0.1f);
         dirRight = Mathf.SmoothDamp(dirRight, targetDirRight, ref velocityRight, 0.1f);
 
-        Dirmag = Mathf.Sqrt((dirForward * dirForward) + (dirRight * dirRight));
-        Dirvec = dirRight * transform.right + dirForward * transform.forward;
+        //方形数据转圆形，解决斜方向根号2的问题
+        Vector2 tempDirAxis = RectToCircle(new Vector2(dirRight, dirForward));
+        float _dirForward = tempDirAxis.y;
+        float _dirRight = tempDirAxis.x;
+
+        //计算多方向键入的位移量
+        Dirmag = Mathf.Sqrt((_dirForward * _dirForward) + (_dirRight * _dirRight));
+        //计算多方向键入的方向向量，比如 w a 一起按为左前方
+        Dirvec = _dirRight * transform.right + _dirForward * transform.forward;
+
+        run = Input.GetKey(keyA);
+
+        bool tempJump = Input.GetKey(keyB);
+        if(tempJump != lastJump && tempJump)
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+        lastJump = tempJump;
 
 	}
+
+
+
+    private Vector2 RectToCircle(Vector2 input)
+    {
+        Vector2 output = Vector2.zero;
+
+        output.x = input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2.0f);
+        output.y = input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2.0f);
+
+        return output;
+    }
 
     
 }
