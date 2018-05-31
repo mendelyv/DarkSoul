34dd826b209b16df87b455e3cd16a2d56050126d
LORDRAN <1064137887@qq.com>
2018-05-31 21:28:46 +0800

    【简要描述】：新增sensor 【详细描述】： 	1.新增一个sensor空节点 	2.在sensor节点上挂载一个OnGroundSensor的检测脚本，用于检测与地面的碰撞 	3.新增一个Layer “Ground” 【修 改 人】：杨浩然

---
+++ Assets/Scripts/Player/OnGroundSensor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGroundSensor : MonoBehaviour {
+
+    public CapsuleCollider capcol;
+
+    private Vector3 cirPoint1 = Vector3.zero;
+    private Vector3 cirPoint2 = Vector3.zero;
+    private float radius;
+
+	// Use this for initialization
+	void Awake () {
+        radius = capcol.radius;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        cirPoint1 = transform.position + transform.up * radius;
+        cirPoint2 = transform.position + transform.up * capcol.height - transform.up * radius;
+
+        Collider[] outputColliders = Physics.OverlapCapsule(cirPoint1, cirPoint2, radius,LayerMask.GetMask("Ground"));
+        if (outputColliders.Length != 0)
+        {
+
+        }
+	}
+}
