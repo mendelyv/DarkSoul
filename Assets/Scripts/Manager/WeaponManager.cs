0044f31fde1ff0877b221ffe5cc43da15ae3054a
LORDRAN <1064137887@qq.com>
2018-08-29 20:39:46 +0800

    【简要描述】：动画帧开关动画 【详细描述】： 	1.hit状态往Ground状态转变时加入打断属性interuption 	2.给三段攻击动画增加开关武器碰撞器的动画帧事件 	3.增加一个WeaponManager类，用来接收动画事件 	4.傅老师视频第二季第9集 【修 改 人】：杨浩然

---
+++ Assets/Scripts/Manager/WeaponManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+
+    public CapsuleCollider weaponCol;
+
+    public void WeaponEnable()
+    {
+        weaponCol.enabled = true;
+    }
+
+    public void WeaponDisable()
+    {
+        weaponCol.enabled = false;
+    }
+
+
+}
